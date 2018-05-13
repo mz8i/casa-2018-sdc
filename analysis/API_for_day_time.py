de0d2f6423a60b981e04ce2ddea02ceb0770cf69
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2018-05-13 22:16:29 +0100

    Modify sunlight API data download for getting a whole date range

--- analysis/API_for_day_time.py
+++ analysis/API_for_day_time.py
@@ -10,27 +10,36 @@ Created on Sun May 13 20:56:30 2018
 import requests
 import pandas as pd
 
+from datetime import date
+
 #Most requests are made up of three parts - the requested endpoint, the required (and optional, if using) parameters, and authentication details. We're going to use the Requests library to sort this out. Let's break this down.
 
 #First, we define the endpoint, this is a simple web address, with parameters beginning from a question mark...
 
-endpoint = 'https://api.sunrise-sunset.org/json'
-
-#Next, we set up some parameters, which are sent as Python dictionary. Certain parameters are required for each API, and it's your job to figure out what is required. This time I've written this one out for you. In this case, we're going to extract the keywords within a website.
-
-params = {
-        'lat': 41.8781, 
-        'lng': -87.6298,
-        'date': '2017-02-08',
-        'formatted': 0
+def get_sunlight_for_date(d):
+        endpoint = 'https://api.sunrise-sunset.org/json'
+        params = {
+                'lat': 41.8781, 
+                'lng': -87.6298,
+                'date': d.isoformat(),
+                'formatted': 0
         }
 
-r = requests.get(endpoint, params=params)
+        r = requests.get(endpoint, params=params)
 
-#Now check out the response below. Here, the code will tell you different things. Success = 200, Failure = 400+. Good old Wikipedia has a breakdown of the codes here, which can help you identify any problems with your request. You can also add .json() to the request to get the full response.
+        if r.status_code == 200:
+                return r.json()['results']
+        raise ValueError('The API request was not successful')
 
-r.status_code
+start_date = date(2017, 12, 25)
+end_date = date(2017,12,31)
 
-#The response allows us to assess whether we should continue with the processing of the data. So providing you received a response 200 you can continue with checking out the json data returned. The command below extracts the json element of the response.
-
-r.json()
+api_results = []
+date_range = pd.date_range(start_date, end_date)
+date_range_n = len(date_range)
+    
+for ind, d in enumerate(date_range):
+    print("Running request {} of {}...".format(ind+1, date_range_n))
+    api_results.append(get_sunlight_for_date(d))
+    
+sunlight_df = pd.DataFrame(api_results)
