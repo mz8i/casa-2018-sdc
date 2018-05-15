398ef1e343c7adce7afc7c24a6f514ba0b928bb5
jnbaene <39124538+jnbaene@users.noreply.github.com>
2018-05-15 12:41:44 +0100

    Add query to retrieve data for specific crime type

--- server/api/api.js
+++ server/api/api.js
@@ -59,10 +59,76 @@ api.get('/crimes/:year/:primarytype', function (req, res) {
 });
 
 
+//  API EndPoint to get data from a specific type and year, for heatmap draw
+api.get('/crimes/:year/:primarytype', function (req, res) {
+
+        // Alows data to be downloaded from the server with security concerns
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-WithD");
+        // If all the variables are provided connect to the database
+        if (req.params.primarytype != "" && req.params.year) {
+
+                // Parse the values from the URL into numbers for the query
+                var year = parseFloat(req.params.year);
+                var primarytype = req.params.primarytype;
+
+                // SQL Statement to run
+                var sql = "SELECT Crimes.Latitude, Crimes.Longitude FROM Crimes WHERE PrimaryType = " + connection.escape(primarytype) + " and YEAR = " + year + "";
+
+                // Log it on the screen for debugging
+                console.log(sql);
+
+                // Run the SQL Query
+                connection.query(sql, function (err, rows, fields) {
+                        if (err) console.log("Err:" + err);
+                        if (rows != undefined) {
+                                // If we have data that comes bag send it to the user.
+                                res.send(rows);
+                        } else {
+                                res.send("");
+                        }
+                });
+        } else {
+                // If all the URL variables are not passed send an empty string to the user
+                res.send("");
+        }
+});
 
 
+//  API EndPoint to get data for all year , of a specific crime typr
+api.get('/crimes/:primarytype', function (req, res) {
 
+        // Alows data to be downloaded from the server with security concerns
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-WithD");
+        // If all the variables are provided connect to the database
+        if (req.params.primarytype != "" && req.params.year) {
 
+                // Parse the values from the URL into numbers for the query
+                
+                var primarytype = req.params.primarytype;
+
+                // SQL Statement to run
+                var sql = "SELECT Crimes.Date, Crimes.Latitude, Crimes.Longitude, Crimes.Date FROM Crimes WHERE PrimaryType = " + connection.escape(primarytype) + " ";
+
+                // Log it on the screen for debugging
+                console.log(sql);
+
+                // Run the SQL Query
+                connection.query(sql, function (err, rows, fields) {
+                        if (err) console.log("Err:" + err);
+                        if (rows != undefined) {
+                                // If we have data that comes bag send it to the user.
+                                res.send(rows);
+                        } else {
+                                res.send("");
+                        }
+                });
+        } else {
+                // If all the URL variables are not passed send an empty string to the user
+                res.send("");
+        }
+});
 
 
 module.exports = api;
